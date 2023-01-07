2d907de01ba1ea241c8b44ce0edc06a690619c1b
Phil Wang <lucidrains@gmail.com>
2023-01-07 10:57:15 -0800

    one of the authors emailed me

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'muse-maskgit-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.6',
+  version = '0.0.7',
   license='MIT',
   description = 'MUSE - Text-to-Image Generation via Masked Generative Transformers, in Pytorch',
   author = 'Phil Wang',
   author_email = 'lucidrains@gmail.com',
   long_description_content_type = 'text/markdown',
-  url = 'https://github.com/lucidrains/muse-pytorch',
+  url = 'https://github.com/lucidrains/muse-maskgit-pytorch',
   keywords = [
     'artificial intelligence',
     'deep learning',
