a8139f1fb8312c26bbedbfc6f67307166fc8a477
isamu-isozaki <isamu.website@gmail.com>
2023-02-19 21:09:07 -0500

    Cleaned up mask git training

--- train_muse_maskgit.py
+++ train_muse_maskgit.py
@@ -18,10 +18,37 @@ def parse_args():
     # Create the parser
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--max_grad_norm", type=float, default=None, help="Max gradient norm."
+        "--num_tokens", type=int, default=256, help="Number of tokens. Must be same as codebook size above"
     )
     parser.add_argument(
-        "--discr_max_grad_norm", type=float, default=None, help="Max gradient norm for discriminator."
+        "--seq_len", type=int, default=1024, help="The sequence length. Must be equivalent to fmap_size ** 2 in vae"
+    )
+    parser.add_argument(
+        "--dim", type=int, default=128, help="Model dimension"
+    )
+    parser.add_argument(
+        "--depth", type=int, default=2, help="The depth of model"
+    )
+    parser.add_argument(
+        "--dim_head", type=int, default=64, help="Attention head dimension"
+    )
+    parser.add_argument(
+        "--head", type=int, default=8, help="Attention heads"
+    )
+    parser.add_argument(
+        "--ff_mult", type=int, default=4, help="Feed forward expansion factor"
+    )
+    parser.add_argument(
+        "--t5_name", type=str, default="t5-small", help="Name of your t5 model"
+    )
+    parser.add_argument(
+        "--cond_image_size", type=int, default=None, help="Conditional image size."
+    )
+    parser.add_argument(
+        "--validation_prompt", type=str, default="A photo of a dog", help="Validation prompt."
+    )
+    parser.add_argument(
+        "--max_grad_norm", type=float, default=None, help="Max gradient norm."
     )
     parser.add_argument(
         "--seed", type=int, default=42, help="Seed."
@@ -142,18 +169,20 @@ def main():
         dataset = get_dataset_from_dataroot(args.train_data_dir, args)
     elif args.dataset_name:
         dataset = load_dataset(args.dataset_name)
+
+
     vae = VQGanVAE(
         dim = args.dim,
         vq_codebook_size = args.vq_codebook_size
     ).cuda()
-    
+
     print ('Resuming VAE from: ', args.resume_from)
     vae.load(args.resume_from)    # you will want to load the exponentially moving averaged VAE
-    
+
     # then you plug the vae and transformer into your MaskGit as so
-    
+
     # (1) create your transformer / attention network
-    
+
     transformer = MaskGitTransformer(
         num_tokens = args.num_tokens,         # must be same as codebook size above
         seq_len = args.seq_len,               # must be equivalent to fmap_size ** 2 in vae
@@ -164,38 +193,41 @@ def main():
         ff_mult = args.ff_mult,               # feedforward expansion factor
         t5_name = args.t5_name,               # name of your T5
     )
-    
+
     # (2) pass your trained VAE and the base transformer to MaskGit
-    
+
     maskgit = MaskGit(
         vae = vae,                 # vqgan vae
         transformer = transformer, # transformer
         image_size = args.image_size,          # image size
         cond_drop_prob = args.cond_drop_prob,     # conditional dropout, for classifier free guidance
+        cond_image_size = args.cond_image_size
     ).cuda()
     dataset = ImageTextDataset(dataset, args.image_size, transformer.tokenizer, image_column=args.image_column, caption_column=args.caption_column)
+    dataloader, validation_dataloader = split_dataset_into_dataloaders(dataset, args.valid_frac, args.seed)
 
     trainer = MaskGitTrainer(
         maskgit,\
+        dataloader,
+        validation_dataloader,
+        current_step=0,
         num_train_steps=args.num_train_steps,
         batch_size=args.batch_size,
         image_size=args.image_size,  # you may want to start with small images, and then curriculum learn to larger ones, but because the vae is all convolution, it should generalize to 512 (as in paper) without training on it
         lr=args.lr,
-        max_grad_norm=None,
-        discr_max_grad_norm=None,
+        max_grad_norm=args.max_grad_norm,
         save_results_every=args.save_results_every,
         save_model_every=args.save_model_every,
         results_dir=args.results_dir,
         logging_dir=args.logging_dir,
-        valid_frac=0.05,
-        random_split_seed=42,
-        use_ema=True,
-        ema_beta=0.995,
-        ema_update_after_step=1,
-        ema_update_every=1,
-        apply_grad_penalty_every=4,
+        use_ema=args.use_ema,
+        ema_beta=args.ema_beta,
+        ema_update_after_step=args.ema_update_after_step,
+        ema_update_every=args.ema_update_every,
+        apply_grad_penalty_every=args.apply_grad_penaly_every,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
-        mixed_precesion=args.mixed_precision,
+        mixed_precision=args.mixed_precision,
+        validation_prompt=args.validation_prompt
     )
 
     trainer.train()
