ea0e421793d4e366aa1bd3d2363c1952cb8f049f
wnakano <nknvolpi@gmail.com>
2023-03-04 02:11:26 -0300

    feat: adding VQGanVAETaming

--- muse_maskgit_pytorch/__init__.py
+++ muse_maskgit_pytorch/__init__.py
@@ -1,4 +1,5 @@
 from muse_maskgit_pytorch.vqgan_vae import VQGanVAE
+from muse_maskgit_pytorch.vqgan_vae_taming import VQGanVAETaming
 from muse_maskgit_pytorch.muse_maskgit_pytorch import (
     Transformer,
     MaskGit,
