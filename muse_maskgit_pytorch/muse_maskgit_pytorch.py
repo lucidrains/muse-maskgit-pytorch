45b087b241faf8f3e93c16c9bbd3012657b49fb7
Vishnu V Jaddipal <95531133+Gothos@users.noreply.github.com>
2023-03-15 13:18:08 +0530

    Update muse_maskgit_pytorch.py

--- muse_maskgit_pytorch/muse_maskgit_pytorch.py
+++ muse_maskgit_pytorch/muse_maskgit_pytorch.py
@@ -5,7 +5,8 @@ from functools import partial
 import torch
 import torch.nn.functional as F
 from torch import nn, einsum
-
+import Pathlib
+from Pathlib import Path
 import torchvision.transforms as T
 
 from typing import Callable, Optional, List
